3f7aa32c5f08ef236d655eb2c4f2f3eeaebec34b
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-08-13 13:09:16 -0400

    created files and edited mailer.rb to create password recovery and reactivation. NOT COMPLETED

--- mailer.rb
+++ mailer.rb
@@ -20,3 +20,23 @@ class Newsletter < ActionMailer::Base
     mail(to: recipent, subject: 'Thank you for signing up to Aj\'s Environmental Blog')
   end
 end
+
+class Newsletter < ActionMailer::Base
+  default from: "from@example.com"
+  def passwordChange(recipent, passwordChangeCode, last_name)
+    @recipent = recipent
+    @passwordChangeCode = passwordChangeCode
+    @last_name = last_name
+    mail(to: recipent, subject: 'Request for password change to Aj\'s Environmental Blog')
+  end
+end
+
+class Newsletter < ActionMailer::Base
+  default from: "from@example.com"
+  def reactivate(recipent, reactivationCode, last_name)
+    @recipent = recipent
+    @reactivationCode = reactivationCode
+    @last_name = last_name
+    mail(to: recipent, subject: 'Welcome back to Aj\'s Environmental Blog')
+  end
+end
